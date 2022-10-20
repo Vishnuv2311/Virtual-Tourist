257916419e2a81bf670c1db80cf74bf7b04854a5
Vishnuv2311 <emailvishnuv@gmail.com>
2022-10-20 09:42:42 +0530

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Vishnu V on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
